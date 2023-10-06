2cd60b41fc73066fe8282b9590dc78936f4a2cd4
Kokanov Alexandr <kokanov.av@students.dvfu.ru>
2023-10-06 22:38:32 +1000

    first commit

---
+++ ToDoList/Controllers/ApiController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ToDoList.Models;
+
+namespace ToDoList.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApiController : ControllerBase
+    {
+        private readonly NotesDbContext dbContext;
+
+        public ApiController(NotesDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok(await dbContext.Notes.ToListAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get([FromRoute] int id)
+        {
+            var note = await dbContext.Notes.FindAsync(id);
+            
+            if (note == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(await dbContext.Notes.FindAsync(id));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(Note note)
+        {
+            await dbContext.Notes.AddAsync(note);
+            await dbContext.SaveChangesAsync();
+
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete([FromRoute] int id)
+        {
+            var note = await dbContext.Notes.FindAsync(id);
+
+            if (note == null)
+            {
+                return NotFound();
+            }
+
+            dbContext.Remove(note);
+            await dbContext.SaveChangesAsync();
+            return Ok();
+
+        }
+    }
+}
---
+++ ToDoList/Models/NotesDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ToDoList.Models
+{
+    public class NotesDbContext : DbContext
+    {
+        public NotesDbContext(DbContextOptions<NotesDbContext> options)
+            : base(options) { }
+
+        public DbSet<Note> Notes => Set<Note>();
+    }
+}
